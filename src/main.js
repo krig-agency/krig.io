c8a5ee3a59c56b5f72cc0f86bf5e19e6ff03a1f4
razitazi <razi@razitazi.com>
2021-12-26 11:23:32 +0100

    Cleaned out some unused components causing errors

--- src/main.js
+++ src/main.js
@@ -2,16 +2,14 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
-import AnimeJS from 'vue-plugin-animejs'
-import ToggleSwitch from 'vuejs-toggle-switch'
-import VueResizeText from 'vue-resize-text';
+// import AnimeJS from 'vue-plugin-animejs'
+// import ToggleSwitch from 'vuejs-toggle-switch'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
-  Vue.use(AnimeJS)
-  Vue.use(ToggleSwitch)
-  Vue.use(VueResizeText)
+  // Vue.use(AnimeJS)
+  // Vue.use(ToggleSwitch)
 }
 
 // new Vue({
